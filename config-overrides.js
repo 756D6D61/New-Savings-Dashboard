dc0dac8935080d8b12c166d28e88cc85d73320f1
756D6D61 <756D6D61@gmail.com>
2019-11-02 12:02:12 +0000

    react hot reloader

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 /* config-overrides.js */
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
+	config = rewireReactHotLoader(config, env);
+	return config;
 };
