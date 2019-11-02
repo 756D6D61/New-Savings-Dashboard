7428b5a987671594e382dea62dd22e6cd61c3f00
756D6D61 <756D6D61@gmail.com>
2019-11-02 01:10:35 +0000

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonText, setButtonText] = useState('Click me, please');
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => setButtonText('Thanks, been clicked!')}>
+          {buttonText}
+        </button>
       </header>
     </div>
   );
