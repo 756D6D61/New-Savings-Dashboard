c73c37b8ce9ad5c0eaffa1d90b63af5b55990f35
756D6D61 <756D6D61@gmail.com>
2019-12-01 15:42:39 +0000

    Page structure added

--- src/pages/AddAccount/index.js
+++ src/pages/AddAccount/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
+import Page from '../../layout/Page'
 
 const AddAccount = () => {
 	return (
-		<div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-			<div>Add Account</div>
-		</div>
+		<Page>
+			<div class="flex flex-wrap">
+				<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+					Add Account
+				</div>
+			</div>
+		</Page>
 	)
 }
 
--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Page from '../../layout/Page'
 
 const Categories = () => {
-	return <div>Categories</div>
+	return (
+		<Page>
+			<div class="flex flex-wrap">
+				<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+					Categories
+				</div>
+			</div>
+		</Page>
+	)
 }
 
 export default Categories
