ffe48eb233da69f549229850fab142dcb6102447
756D6D61 <756D6D61@gmail.com>
2019-12-03 14:07:44 +0000

    Font awesome icons

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Page from '../../layout/Page'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Categories = () => {
 	return (
@@ -7,6 +9,7 @@ const Categories = () => {
 			<div class="flex flex-wrap">
 				<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
 					Categories
+					<FontAwesomeIcon icon={faCoffee} />
 				</div>
 			</div>
 		</Page>
