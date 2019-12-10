eeb37499d697607922fc721906d1337666ec8cd2
756D6D61 <756D6D61@gmail.com>
2019-12-10 13:00:48 +0000

    Button change

--- src/components/Form.js
+++ src/components/Form.js
@@ -142,7 +142,7 @@ const AccForm = () => {
 				</label>
 			</div>
 			<div>
-				<button>Save</button>
+				<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
 			</div>
 		</div>
 	)
