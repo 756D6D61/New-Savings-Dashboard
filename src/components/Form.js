bf08af28ca0040b2fe78dbffb9eb73533c606e6c
756D6D61 <756D6D61@gmail.com>
2019-12-10 11:38:28 +0000

    Form complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,68 +3,11 @@ import React from 'react'
 const AccForm = () => {
 	return (
 		<div>
-			<form class="w-full max-w-sm">
-				<div class="flex items-center border-b border-b-2 border-teal-500 py-2">
-					<input
-						class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-						type="text"
-						placeholder="Jane Doe"
-						aria-label="Full name"
-					/>
-					<button
-						class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-						type="button"
-					>
-						Sign Up
-					</button>
-					<button
-						class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
-						type="button"
-					>
-						Cancel
-					</button>
-				</div>
-			</form>
-			<div class="block">
-				<span class="text-gray-700">Checkboxes</span>
-				<div class="mt-2">
-					<div>
-						<label class="inline-flex items-center">
-							<input
-								type="checkbox"
-								class="form-checkbox text-indigo-600"
-								checked
-							/>
-							<span class="ml-2">Option 1</span>
-						</label>
-					</div>
-					<div>
-						<label class="inline-flex items-center">
-							<input
-								type="checkbox"
-								class="form-checkbox text-green-500"
-								checked
-							/>
-							<span class="ml-2">Option 2</span>
-						</label>
-					</div>
-					<div>
-						<label class="inline-flex items-center">
-							<input
-								type="checkbox"
-								class="form-checkbox text-pink-600"
-								checked
-							/>
-							<span class="ml-2">Option 3</span>
-						</label>
-					</div>
-				</div>
-			</div>
 			<label class="block">
-				<span class="text-gray-700">Name</span>
+				<span class="text-gray-700">Account Name Name</span>
 				<input
 					class="form-input mt-1 block w-full"
-					placeholder="Jane Doe"
+					placeholder="Name of Account"
 				/>
 			</label>
 
@@ -76,40 +19,130 @@ const AccForm = () => {
 							type="radio"
 							class="form-radio"
 							name="accountType"
-							value="personal"
+							value="savings"
 						/>
-						<span class="ml-2">Personal</span>
+						<span class="ml-2">Savings</span>
 					</label>
 					<label class="inline-flex items-center ml-6">
 						<input
 							type="radio"
 							class="form-radio"
 							name="accountType"
-							value="busines"
+							value="currentAccount"
 						/>
-						<span class="ml-2">Business</span>
+						<span class="ml-2">Current Account</span>
+					</label>
+					<label class="inline-flex items-center ml-6">
+						<input
+							type="radio"
+							class="form-radio"
+							name="accountType"
+							value="isa"
+						/>
+						<span class="ml-2">ISA</span>
 					</label>
 				</div>
 			</div>
 
 			<label class="block mt-4">
-				<span class="text-gray-700">Requested Limit</span>
+				<span class="text-gray-700">Category</span>
 				<select class="form-select mt-1 block w-full">
-					<option>$1,000</option>
-					<option>$5,000</option>
-					<option>$10,000</option>
-					<option>$25,000</option>
+					<option>Birthday</option>
+					<option>Wedding</option>
+					<option>Holiday</option>
+					<option>Home</option>
 				</select>
 			</label>
 
-			<div class="flex mt-6">
-				<label class="flex items-center">
-					<input type="checkbox" class="form-checkbox" />
-					<span class="ml-2">
-						I agree to the{' '}
-						<span class="underline">privacy policy</span>
-					</span>
+			<div class="block">
+				<span class="text-gray-700">Budgets</span>
+				<div class="mt-2">
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-indigo-600"
+						/>
+						<span class="ml-2">Music</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-green-500"
+							checked
+						/>
+						<span class="ml-2">Phone</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-pink-600"
+							checked
+						/>
+						<span class="ml-2">Health</span>
+					</label>
+
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-indigo-600"
+							checked
+						/>
+						<span class="ml-2">Shopping</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-green-500"
+							checked
+						/>
+						<span class="ml-2">Security</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-pink-600"
+							checked
+						/>
+						<span class="ml-2">Gaming</span>
+					</label>
+
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-indigo-600"
+							checked
+						/>
+						<span class="ml-2">Internet</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-green-500"
+							checked
+						/>
+						<span class="ml-2">Home</span>
+					</label>
+					<label class="inline-flex items-center">
+						<input
+							type="checkbox"
+							class="form-checkbox text-pink-600"
+							checked
+						/>
+						<span class="ml-2">Misc.</span>
+					</label>
+				</div>
+			</div>
+			<div>
+				<label class="block">
+					<span class="text-gray-700">Savings Per Month</span>
+					<input
+						class="form-input mt-1 block w-full"
+						placeholder="Enter amount here"
+					/>
 				</label>
+			</div>
+			<div>
+				<button>Save</button>
 			</div>
 		</div>
 	)
