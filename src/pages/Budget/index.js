0a86fd03156ab61928042c650d117b3c032fb786
756D6D61 <756D6D61@gmail.com>
2019-12-15 02:02:03 +0000

    Budget layout

--- src/pages/Budget/index.js
+++ src/pages/Budget/index.js
@@ -7,12 +7,12 @@ const Budget = () => {
 	return (
 		<Page>
 			<div class="flex flex-wrap">
-				<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500">
+				<div class="w-full  mb-4 text-center bg-gray-500">
 					<AddButton>Budget</AddButton>
 				</div>
 			</div>
 			<div class="flex flex-wrap">
-				<div class="w-full mb-4 bg-gray-500">
+				<div class="w-full mb-4 text-center bg-gray-500">
 					<Icons />
 				</div>
 			</div>
