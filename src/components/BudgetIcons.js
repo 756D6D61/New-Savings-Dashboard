4a74120d84b2a3dcdc3d19b73718534772b807b4
756D6D61 <756D6D61@gmail.com>
2019-12-15 00:53:14 +0000

    Icons added to buttons

--- src/components/BudgetIcons.js
+++ src/components/BudgetIcons.js
@@ -1,39 +1,56 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faHeadphonesAlt,
+	faPhone,
+	faHeartbeat,
+	faShoppingBag,
+	faShieldAlt,
+	faGamepad,
+	faWifi,
+	faHome,
+	faReceipt
+} from '@fortawesome/free-solid-svg-icons'
 const BudgetIcons = () => {
 	return (
 		<div>
 			<div>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faHeadphonesAlt} />
 					Music
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faPhone} />
 					Phone
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-					Health
+					<FontAwesomeIcon icon={faHeartbeat} /> Health
 				</button>
 			</div>
 			<div>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-					Shopping
+					<FontAwesomeIcon icon={faShoppingBag} /> Shopping
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faShieldAlt} />
 					Security
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faGamepad} />
 					Gaming
 				</button>
 			</div>
 			<div>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faWifi} />
 					Internet
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faHome} />
 					Home
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-					Music
+					<FontAwesomeIcon icon={faReceipt} /> Misc.
 				</button>
 			</div>
 		</div>
--- src/components/CategoryIcons.js
+++ src/components/CategoryIcons.js
@@ -1,21 +1,31 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faBirthdayCake,
+	faPhone,
+	faHeart,
+	faHome
+} from '@fortawesome/free-solid-svg-icons'
 const CategoryIcons = () => {
 	return (
 		<div>
 			<div>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faBirthdayCake} />
 					Birthday
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faPhone} />
 					Wedding
 				</button>
 			</div>
 			<div>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faHeart} />
 					Holiday
 				</button>
 				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+					<FontAwesomeIcon icon={faHome} />
 					Home
 				</button>
 			</div>
