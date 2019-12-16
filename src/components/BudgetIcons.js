0f8715ce7242e28076a0f04c6da9e2dcb8c0ad09
756D6D61 <756D6D61@gmail.com>
2019-12-16 14:12:10 +0000

    Category buttons

--- src/components/BudgetIcons.js
+++ src/components/BudgetIcons.js
@@ -15,61 +15,61 @@ const BudgetIcons = () => {
 	return (
 		<div class="flex flex-wrap -mx-1 overflow-hidden sm:-mx-2 md:-mx-2 lg:-mx-2">
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faHeadphonesAlt} />
 					Music
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faPhone} />
 					Phone
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faHeartbeat} /> Health
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faShoppingBag} /> Shopping
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faShieldAlt} />
 					Security
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faGamepad} />
 					Gaming
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faWifi} />
 					Internet
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faHome} />
 					Home
 				</button>
 			</div>
 
 			<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:w-1/3">
-				<button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+				<button class="text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white h-20 w-20 border border-blue-500 hover:border-transparent rounded">
 					<FontAwesomeIcon icon={faReceipt} /> Misc.
 				</button>
 			</div>
