84db61865304dd0b4dbd919880bce9abc2db8c89
756D6D61 <756D6D61@gmail.com>
2019-12-21 10:41:22 +0000

    Tweak to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import Page from '../../layout/Page'
 import AltGraph from '../../components/AltGraph'
 import Table from '../../components/Table'
+import Card from '@material-ui/core/Card'
 
 const Home = ({ route }) => {
 	return (
@@ -9,25 +10,29 @@ const Home = ({ route }) => {
 			<Suspense fallback={<p>Loading</p>}>
 				<div class="flex -mx-2">
 					<div class="w-1/3 px-2  mb-4">
-						<div class="bg-gray-400 h-12">Budgets</div>
+						<Card>
+							<div class="bg-gray-400 h-12">Budgets</div>
+						</Card>
 					</div>
 					<div class="w-1/3 px-2  mb-4">
-						<div class="bg-gray-500 h-12">Categories</div>
+						<Card>
+							<div class="bg-gray-500 h-12">Categories</div>
+						</Card>
 					</div>
 					<div class="w-1/3 px-2  mb-4">
-						<div class="bg-gray-400 h-12">test</div>
+						<Card>
+							<div class="bg-gray-400 h-12">test</div>
+						</Card>
 					</div>
 				</div>
 				<div class="flex flex-wrap -mx-2  mb-4">
 					<div class="w-1/2 px-2">
-						<div class=" h-12">
+						<Card>
 							<AltGraph />
-						</div>
+						</Card>
 					</div>
 					<div class="w-1/2 px-2  mb-4">
-						<div class=" h-12">
-							<Table />
-						</div>
+						<Table />
 					</div>
 				</div>
 			</Suspense>
