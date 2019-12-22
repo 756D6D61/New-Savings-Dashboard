06bd625f9ff14795ccb3b6edd506a69a27858e27
756D6D61 <756D6D61@gmail.com>
2019-12-22 23:39:16 +0000

    Code clean up

--- src/layout/page.js
+++ src/layout/page.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import NavTest from '../components/NavTest'
-import Container from '@material-ui/core/Container'
 import PropTypes from 'prop-types'
 
 const propTypes = {
@@ -11,7 +10,6 @@ const Page = ({ route, children }) => {
 	return (
 		<div class="flex content-between flex-wrap  h-20">
 			<NavTest route={route} />
-
 			<div class="w-full h-2  pl-20 pr-2">{children}</div>
 		</div>
 	)
