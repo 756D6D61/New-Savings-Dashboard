d643678d49ebe333f12550df9439571f6b05acdf
756D6D61 <756D6D61@gmail.com>
2020-01-05 14:51:09 +0000

    White background removed

--- src/components/SubDivider.js
+++ src/components/SubDivider.js
@@ -7,7 +7,7 @@ const propTypes = {
 
 const SubDivider = ({ children }) => {
 	return (
-		<div class="bg-white w-full -mt-5 z-0 font-light -ml-1 text-2xl">
+		<div class=" w-full -mt-5 z-0 font-light -ml-1 text-2xl">
 			<div class="pt-5 pb-2">{children}</div>
 
 			<hr />
