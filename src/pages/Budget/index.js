95e4e60325414ac5a2530460687e1fde617cfb32
756D6D61 <756D6D61@gmail.com>
2020-02-05 07:10:05 +0000

    Added Redux

--- src/pages/Budget/index.js
+++ src/pages/Budget/index.js
@@ -1,11 +1,22 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import Page from '../../layout/Page'
 import AddButton from '../../components/AddButton'
 import Icons from '../../components/Budgets/BudgetIcons'
 import Card from '@material-ui/core/Card'
 import SubDivider from '../../components/Home/SubDivider'
+import { useSelector, useDispatch } from 'react-redux'
+import { budgetData } from '../../store/actions/data.actions'
 
 const Budget = () => {
+	const dispatch = useDispatch()
+	const budgets = useSelector(state => state.data.budget)
+
+	useEffect(() => {
+		dispatch(budgetData())
+	}, [dispatch])
+
+	console.log(budgets)
+
 	return (
 		<Page>
 			<Suspense fallback={<p>Loading</p>}>
