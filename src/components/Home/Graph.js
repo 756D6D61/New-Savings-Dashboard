ff128f6c97719c6e5320d7093c56b4d769e3829a
756D6D61 <756D6D61@gmail.com>
2020-03-13 15:17:53 +0000

    X-axis labels

--- src/components/Home/Graph.js
+++ src/components/Home/Graph.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import { useSelector, useDispatch } from 'react-redux'
 import { categoryData } from '../../store/actions/data.actions'
+import moment from 'moment'
 
 const Graph = () => {
 	const dispatch = useDispatch()
@@ -21,50 +22,6 @@ const Graph = () => {
 
 	console.log(test)
 
-	const data = [
-		{
-			name: 'Page A',
-			uv: 4000,
-			pv: 2400,
-			amt: 2400
-		},
-		{
-			name: 'Page B',
-			uv: 3000,
-			pv: 1398,
-			amt: 2210
-		},
-		{
-			name: 'Page C',
-			uv: 2000,
-			pv: 9800,
-			amt: 2290
-		},
-		{
-			name: 'Page D',
-			uv: 2780,
-			pv: 3908,
-			amt: 2000
-		},
-		{
-			name: 'Page E',
-			uv: 1890,
-			pv: 4800,
-			amt: 2181
-		},
-		{
-			name: 'Page F',
-			uv: 2390,
-			pv: 3800,
-			amt: 2500
-		},
-		{
-			name: 'Page G',
-			uv: 3490,
-			pv: 4300,
-			amt: 2100
-		}
-	]
 	const result = Object.keys(categories).reduce(function (r, k) {
         return r.concat(categories[k]);
 	}, []);
@@ -100,7 +57,8 @@ const Graph = () => {
 		<XAxis
 			interval={0}
 			angle={-65}
-			dataKey={test100}
+			dataKey="timestamp"
+			tickFormatter={(tick) => moment(tick).format('MMM Do YYYY')}
 		/>
 		<YAxis />
 		<Tooltip />
