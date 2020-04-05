ab5319d6a915119e12ace7e523477982f32b4611
Umma Gohil <756D6D61@gmail.com>
2020-04-05 01:26:55 +0100

    Code clean up

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,5 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers/'
 
 export default function configureStore() {
-	return createStore(rootReducer, applyMiddleware(thunk , logger))
+	return createStore(rootReducer, applyMiddleware(thunk, logger))
 }
