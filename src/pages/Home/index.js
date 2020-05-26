dc9fb97749ca984405d38b2306531c5cc0facaa7
u-gohil <756D6D61@gmail.com>
2020-05-26 22:02:06 +0100

    Removed react-rewired

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import Page from '../../layout/page'
+import Page from '../../layout/newPage'
 import AltGraph from '../../components/Home/Graph'
 import Table from '../../components/Home/TableTwo'
 import Card from '@material-ui/core/Card'
