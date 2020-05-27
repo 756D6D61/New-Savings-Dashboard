8dab1ee62f7889789ccadfd8527ed62f296accad
u-gohil <756D6D61@gmail.com>
2020-05-27 08:31:40 +0100

    Page done

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -5,12 +5,16 @@ import AddButton from '../../components/AddButton'
 import Icons from '../../components/Categories/CategoryIcons'
 import Card from '@material-ui/core/Card'
 import SubDivider from '../../components/Home/SubDivider'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBorderAll } from '@fortawesome/free-solid-svg-icons'
 
 const Categories = () => {
 	return (
 		<Page>
 			<Suspense fallback={<p>Loading</p>}>
-				<SubDivider>Categories</SubDivider>
+				<SubDivider>
+					<FontAwesomeIcon icon={faBorderAll} size="sm" /> Categories
+				</SubDivider>
 				<br />
 				<div class="flex flex-wrap">
 					<div class="w-full text-center mb-4">
@@ -20,13 +24,13 @@ const Categories = () => {
 				<div class="flex -mx-2">
 					<div class="w-1/2 px-2 mb-4">
 						<Card>
-							<div class="text-center ">
+							<div class="text-center shadow-md bg-red-400 rounded p-3">
 								<Icons />
 							</div>
 						</Card>
 					</div>
 					<div class="w-1/2 px-2  mb-4">
-						<div class="bg-gray-500 text-center h-12">
+						<div class="text-center shadow-md bg-red-400 rounded p-3">
 							<Table />
 						</div>
 					</div>
